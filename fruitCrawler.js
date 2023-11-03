62b9bdeea6a5774bf81a917c53a15489666394dc
raghadhamida <120336521+raghadhamida@users.noreply.github.com>
2023-11-03 18:47:14 -0400

    Add files via upload

---
+++ fruitCrawler.js
@@ -0,0 +1,198 @@
+
+import crawler from 'crawler';
+import {Matrix} from "ml-matrix";
+
+import elasticlunr from 'elasticlunr';
+
+//Create your index
+//Specify fields you want to include in search
+//Specify reference you want back (i.e., page ID)
+const index = elasticlunr(function () {
+    this.addField('title');
+    this.addField('content');
+    this.setRef('id');
+});
+
+let crawledPages = new Set();
+let pagesQueue = new Set();
+
+
+//importing collections
+import Pages from './collections/pages.js';
+
+
+
+const c = new crawler({
+    maxConnections : 10, //use this for parallel, rateLimit for individual
+    //rateLimit: 1000,
+
+    // This will be called for each crawled page
+    callback: async function (error, res, done) {
+        if(error){
+            console.log(error);
+        }else{
+            let url = res.options.uri; //this is the url of the page we're crawling 
+            //console.log(url);
+            if (crawledPages.has(url)){ //this page has been visited before, so skip it 
+                //console.log("Visited this page before: " + url );
+                return;
+            }else{ //this means that the page hasn't been visited before, so extract and process data 
+                
+                crawledPages.add(url);
+                let $ = res.$; //get cheerio data, see cheerio docs for info
+                let content = $('p').text(); //this is all the fruits 
+                let title = $('title').text();
+                let links = $("a") //get all links from page
+                let outgoingLinks = [];
+                $(links).each(function(i, link){
+                    let linkText = $(link).text();
+                    let completeLink = " https://people.scs.carleton.ca/~davidmckenney/fruitgraph/" + linkText + ".html";
+                    //console.log("Complete link: " + completeLink);
+                    outgoingLinks.push(completeLink);
+                });
+
+
+                for (const outgoingLink of outgoingLinks) {
+                    if (!pagesQueue.has(outgoingLink)) {
+                        c.queue(outgoingLink);
+                        pagesQueue.add(outgoingLink);
+                        //console.log(outgoingLink + " added to queue");
+                    }
+    
+                    try {
+                        await Pages.findOneAndUpdate(
+                            { url: outgoingLink },
+                            { $addToSet: { incomingLinks: url } },
+                            { upsert: true }
+                        );
+                        //console.log(url + " added to list of incoming links of " + outgoingLink);
+                    } catch (err) {
+                        console.log("Error updating incomingLinks: " + err);
+                    }
+                }
+    
+                try {
+                    const updatedPage = await Pages.findOneAndUpdate(
+                        { url: url },
+                        { $set: { title: title, content: content, outGoingLinks: outgoingLinks } },
+                        { upsert: true, new: true },
+                    );
+                    
+                    index.addDoc({
+                        id: updatedPage._id,  // Using the database ID as the reference
+                        title: title,
+                        content: content
+                    });
+                    
+                    //console.log("Page updated:", updatedPage);
+                } catch (err) {
+                    console.log("Error creating/updating document for " + url + ": " + err);
+                }
+
+            }
+            
+        }
+        done();
+    },
+});
+
+c.on("error", function (error) {
+    console.error("Crawler error:", error);
+});
+
+
+
+function generateAdjacencyMatrix(pages) {
+    //console.log(pages);
+    const n = pages.length;
+    console.log("Number of pages:", n);
+    const adjacencyMatrix = Matrix.zeros(n, n);
+    
+    //console.log("Adjacency Matrix:", adjacencyMatrix);
+    pages.forEach((page, i) => {
+        page.outGoingLinks.forEach((link) => {
+            const j = pages.findIndex((p) => p.url === link);
+            if (j !== -1) {
+                adjacencyMatrix.set(i, j, 1); // There is a link from page i to page j
+            }
+        });
+    });
+
+  
+    //console.log(adjacencyMatrix.getRow(0));
+    return adjacencyMatrix;
+}
+
+
+function calculatePageRank(adjacencyMatrix, alpha = 0.1, epsilon = 0.0001) {
+    const n = adjacencyMatrix.rows;
+    
+    //x0 vector matrix 
+    let x0 = new Matrix(1, n);
+    x0.set(0, 0, 1);
+    for (let i = 1; i < n; i++){
+        x0.set(0, i, 0);
+    }
+
+    for (let i = 0; i < n; i++) {
+        const row = adjacencyMatrix.getRow(i);
+        const onesCount = row.reduce((count, element) => count + element, 0);
+    
+        if (onesCount === 0) {
+          // If the row has no 1s, replace each element by 1/N
+          for (let j = 0; j < n; j++) {
+            adjacencyMatrix.set(i, j, 1 / n);
+          }
+        } else {
+          // If the row has 1s, divide each 1 by the number of 1s in that row
+          for (let j = 0; j < n; j++) {
+            if (row[j] === 1) {
+                adjacencyMatrix.set(i, j, 1 / onesCount);
+            }
+          }
+        }
+    }
+
+    //multiply adjacencyMatrix by (1 - alpha)
+    adjacencyMatrix.mul(1-alpha); 
+
+    //Add alpha/N to each entry	of the adjacencyMatrix
+    for (let i = 0; i < n; i++){
+        for (let j = 0; j < n; j++){
+            let currValue = adjacencyMatrix.get(i, j);
+            adjacencyMatrix.set(i, j, currValue + (alpha/n));
+        }
+    }
+
+    //console.log(adjacencyMatrix);
+
+    //implementation of the power iteration
+    let iteration = 1;
+    let oldX0;
+
+    do {
+        oldX0 = x0.clone();
+        x0 = x0.mmul(adjacencyMatrix);
+
+        let difference = x0.clone().sub(oldX0);
+        let euclideanDist = difference.norm();
+
+        if(euclideanDist < epsilon) {
+            console.log("Stopped at iteration #" + iteration + "\n");
+            break;
+        }
+
+        iteration++;
+    } while (true);
+
+    
+
+    return x0;
+
+
+}
+
+
+
+export { c, Pages, generateAdjacencyMatrix, calculatePageRank, index };
+
---
+++ server.js
@@ -0,0 +1,152 @@
+import express from 'express';
+let app = express();
+const port = 3000;
+
+import session from 'express-session';
+import { default as connectMongoDBSession} from 'connect-mongodb-session';
+import { c, Pages, generateAdjacencyMatrix, calculatePageRank, index } from './fruitCrawler.js';
+
+const MongoDBStore = connectMongoDBSession(session);
+
+//Defining the location of the sessions data in the database.
+var store = new MongoDBStore({
+  uri: 'mongodb://127.0.0.1:27017/lab3',
+  collection: 'sessions'
+});
+ 
+
+//Setting up the express sessions and for it to be stored in the database
+app.use(session({
+  secret: 'some secret key here',
+  resave: true,
+  saveUninitialized: false,
+  store: store 
+}));
+
+//Import mongoose module
+import pkg from 'mongoose';
+
+const { connect, Types } = pkg;
+
+app.use(express.urlencoded({extended: true}));
+
+//View engine
+app.set('views', './views');
+app.set("view engine", "pug");
+app.use(express.static("js"));
+
+// Connect to the MongoDB database
+connect('mongodb://127.0.0.1:27017/lab3', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+
+//this converts any JSON stringified string to JSON
+app.use(express.json());
+
+
+
+//Queue a URL, which starts the crawl
+c.queue('https://people.scs.carleton.ca/~davidmckenney/fruitgraph/N-0.html');
+
+
+//serve GET request for /fruits
+app.get("/fruits", async (req, res, next) => {
+    res.status(200).render("search.pug", {web: "Fruits Web Page"});
+});
+
+//serve GET request for /search
+app.get("/search", async (req, res, next) => {
+    try {
+        let searchQuery = req.query.q || '';
+        const boost = req.query.boost === 'true'; // Check if boosting is enabled
+        const limit = parseInt(req.query.limit) || 10; // Get the limit parameter and default to 10 if not provided
+        
+        let indexResults = index.search(searchQuery, {}).slice(0, limit);
+        let numberOfResultsToFetch = limit - indexResults.length;
+
+        // Fetch more results if needed to fulfill the limit
+        if (numberOfResultsToFetch > 0) {
+            const additionalResults = index.search(searchQuery, {}).slice(limit, limit + numberOfResultsToFetch);
+            indexResults = indexResults.concat(additionalResults);
+        }
+        console.log(indexResults);
+        if (boost) {
+            // Sort the indexResults based on the combined score of index and pagerank
+            indexResults.sort(async (a, b) => {
+                const pageA = await Pages.findById(a.ref);
+                const pageB = await Pages.findById(b.ref);
+                //console.log(pageA);
+                //console.log(pageA.pr);
+                
+                // Calculate combined score (index score * pagerank)
+                const scoreA = a.score * pageA.pr;
+                const scoreB = b.score * pageB.pr;
+                //console.log(a.score);
+                //console.log(scoreA);
+                
+                // Sort in descending order
+                return scoreB - scoreA;
+            });
+        }
+
+        let modifiedResults = indexResults.map(async (indexResult) => {
+            try {
+                let page = await Pages.findById(indexResult.ref);
+                indexResult.title = page.title;
+                indexResult.content = page.content;
+                return indexResult;
+            } catch (err) {
+                console.log("Error finding document with query result ID: " + err);
+                throw err; // Propagate the error to be caught by the catch block below
+            }
+        });
+
+        //let topPages = await Promise.all(modifiedResults);
+        //console.log(modifiedResults);
+        // res.format({
+        //     "text/html": () => {res.status(200).render("searchResults.pug", {results: modifiedResults})},
+        //     "application/json": () => {res.status(200).json(results)}
+        // });
+    } catch (err) {
+        console.log("Error processing search request: " + err);
+        res.status(500).send('Internal server error.');
+    }
+});
+
+c.on('drain',async function(){
+    console.log("\n CRAWLING COMPLETED. \n");
+
+    const pages = await Pages.find();
+    //console.log(pages);
+
+    // Generate adjacency matrix
+    const adjacencyMatrix = generateAdjacencyMatrix(pages);
+
+    // Calculate PageRank values
+    const x0 = calculatePageRank(adjacencyMatrix);
+
+    // Update pages in the database with their PageRank scores
+    for (let i = 0; i < pages.length; i++) {
+        pages[i].pr = x0.get(0, i);
+        await pages[i].save(); // Save the updated page with the PageRank score
+    }
+
+    // Sort pages based on PageRank values
+    const rankedPages = pages
+        .filter(page => page.pr !== undefined && page.pr !== null) // Filter out undefined or null values
+        .sort((a, b) => b.pr - a.pr);
+    //console.log(pages);
+    // Print top 25 pages with their ranks and URLs
+    //console.log("Top 25 Pages by PageRank:");
+    for (let i = 0; i < 25; i++) {
+        console.log(`#${i + 1}. (${rankedPages[i].pr.toFixed(10)}) ${rankedPages[i].url}`);
+    }
+
+
+
+    // Start the express server after crawling and PageRank calculation is completed
+    app.listen(3000);
+    console.log("Listening on port 3000.");
+});
